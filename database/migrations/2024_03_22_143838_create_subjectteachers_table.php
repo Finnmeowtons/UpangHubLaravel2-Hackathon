1f5a1c70fc81de9546ee6f61cadda23e5bcd406b
Finnmeowtons <sroiano.christianjose.m@gmail.com>
2024-03-22 23:55:53 +0800

    wasd

--- database/migrations/2024_03_22_143838_create_subjectteachers_table.php
+++ database/migrations/2024_03_22_143838_create_subjectteachers_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('subjectteachers', function (Blueprint $table) {
             $table->id();
             $table->string("subject_code");
+            $table->string("subject_name");
             $table->string("teacher");
             $table->timestamps();
         });
